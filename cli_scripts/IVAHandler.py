1f50fe909298426bf8b2a3634ca48b62bf7da5b2
Bemol <36547531+doubleBemol@users.noreply.github.com>
2021-06-30 14:00:11 -0500

    Update IVAHandler.py

--- cli_scripts/IVAHandler.py
+++ cli_scripts/IVAHandler.py
@@ -1,71 +1,71 @@
-class IVAHandler:
-    """Simple class that can add or substract iva to prices.
-    
-    How to use: Import it to your script by using 
-    
-    from IVAHandler import * (assuming the file is in the same directory),
-    
-    then just instantiate an object
-    
-    iva = IVAHandler() and use iva to performe your calculations.
-    
-    example:
-    result = iva.price_without_iva(122.50)
+class PriceTaxHelper:
+    """Simple class that can add or substract TAX to prices.
+
+    How to use: Import it to your script by using:
+    from PriceTaxHelper import * (assuming the file is in the same directory), 
+
+    Then just instantiate an object:
+    tax = PriceTaxHelper() 
+
+    And use tax to performe your calculations.
+
+    Example:
+    result = tax.price_without_tax(122.50)
     """
 
     def __init__(self):
-        self._IVA_RATE = 0.16
+        self._TAX_RATE = 0.16
 
-    def price_without_iva(self, price_with_iva: float) -> float:
-        """Given a price with the IVA already added, returns the price without it. The IVA rate is obtained from _IVA_RATE.
-
-        Args:
-            price_with_iva (float): The price
-
-        Returns:
-            float: A price without the IVA rate, i.e., the original price.
-        """
-        return price_with_iva / (1 + self._IVA_RATE)
-
-    def price_with_iva(self, price_without_iva: float) -> float:
-        """Given a price without the IVA, returns the price with the IVA. The IVA rate is obtained from _IVA_RATE.
+    def price_without_tax(self, price_with_tax: float) -> float:
+        """Given a price with the TAX already added, returns the price without it. The TAX rate is obtained from _TAX_RATE.
 
         Args:
-            price_without_iva (float): The price.
+            price_with_tax (float): The price
 
         Returns:
-            float: A price with the IVA added.
+            float: A price without the TAX rate, i.e., the original price.
         """
-        return price_without_iva * self._IVA_RATE + price_without_iva
+        return price_with_tax / (1 + self._TAX_RATE)
+
+    def price_with_tax(self, price_without_tax: float) -> float:
+        """Given a price without the TAX, returns the price with the TAX. The TAX rate is obtained from _TAX_RATE.
+
+        Args:
+            price_without_tax (float): The price.
+
+        Returns:
+            float: A price with the TAX added.
+        """
+        return price_without_tax * self._IVA_RATE + price_without_tax
 
     @property
-    def iva_rate(self) -> float:
-        """Returns the internal IVA rate, so you can know what value is being used
+    def tax_rate(self) -> float:
+        """Returns the internal TAX rate, so you can know what value is being used
 
         Returns:
-            float: The current IVA rate.
+            float: The current TAX rate.
         """
-        return self._IVA_RATE
+        return self._TAX_RATE
 
-    @iva_rate.setter
-    def iva_rate(self, new_iva_rate: float) -> None:
-        """It allow to set a new IVA rate. The new value must be in a decimal format, if not, a convertion will be attempted.
+    @tax_rate.setter
+    def tax_rate(self, new_tax_rate: float) -> None:
+        """It allow to set a new TAX rate. The new value must be in a decimal format, if not, a convertion will be attempted.
 
         Args:
-            new_iva_rate (float): The decimal value of the new IVA rate.
+            new_tax_rate (float): The decimal value of the new TAX rate.
 
         Raises:
             TypeError: The entering value must be a decimal (float).
         """
-        isFloat = isinstance(new_iva_rate, float)
+        isFloat = isinstance(new_tax_rate, float)
         try:
             if not isFloat:
-                raise TypeError("IVa rate must be provided in decimals")
-            self._IVA_RATE = new_iva_rate
+                raise TypeError("TAX rate must be provided in decimals")
+            self._TAX_RATE = new_tax_rate
         except TypeError:
             try:
-                new_iva_rate = new_iva_rate / 100
-                self._IVA_RATE = new_iva_rate
+                new_tax_rate = new_tax_rate / 100
+                self._TAX_RATE = new_tax_rate
                 print("Value was converted to decimal form and saved correctly!")
             except:
                 print("Something went wrong with the conversion. Nothing was assigned!")
